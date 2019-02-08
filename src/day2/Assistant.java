408c1c63564f89d8bb8fc079856facd53b1a0bc1
NSUIT-LAB <naimur.rahman18@northsouth.edu>
2019-02-08 17:34:08 +0600

    end of day 2

---
+++ src/day2/Assistant.java
@@ -0,0 +1,13 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Assistant {
+    public static void main(String[] args) {
+        Scanner canon303Lide = new Scanner(System.in);
+        String name;
+        name=canon303Lide.next();
+        System.out.println("Hello,"+name);
+
+    }
+}
--- src/day2/BioData.java
+++ src/day2/BioData.java
@@ -2,8 +2,8 @@ package day2;
 
 public class BioData {
     public static void main(String[] args){
-        System.out.println("Name:NAIMUR");
-        System.out.println("ID:1813344642");
-        System.out.println("Dept:ECE");
+        System.out.print("Name:NAIMUR");
+        System.out.print("ID:1813344642");
+        System.out.print("Dept:ECE");
     }
 }
