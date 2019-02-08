cb64a3d43608e3f34eacda37b4a0d9e344ab4a61
NSUIT-LAB <naimur.rahman18@northsouth.edu>
2019-02-08 16:39:47 +0600

    BioData

---
+++ src/day2/BioData.java
@@ -0,0 +1,9 @@
+package day2;
+
+public class BioData {
+    public static void main(String[] args){
+        System.out.println("Name:NAIMUR");
+        System.out.println("ID:1813344642");
+        System.out.println("Dept:ECE");
+    }
+}
