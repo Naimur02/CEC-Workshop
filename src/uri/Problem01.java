721906b9c9f806e5d2e898c18e5e5bf4fa815aad
NSUIT-LAB <naimur.rahman18@northsouth.edu>
2019-02-08 18:46:03 +0600

    Problemo1

---
+++ src/uri/Problem01.java
@@ -0,0 +1,20 @@
+package uri;
+
+import java.util.Scanner;
+
+public class Problem01 {
+    public static void main(String[] args){
+       Scanner canon303Lide=new Scanner(System.in);
+
+        int A,B,X;
+
+        A = canon303Lide.nextInt();
+        B = canon303Lide.nextInt();
+
+        X = A + B;
+
+        System.out.println("X = "+X);
+
+        canon303Lide.close();
+    }
+}
